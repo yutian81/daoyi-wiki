787eee70f7e66b0c243ac934ff23a8b51313d592
yutian81 <yutian88881@hotmail.com>
2025-12-04 09:48:46 +0800

    再次更新pdf

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -189,7 +189,7 @@ export default hopeTheme({
       ],
       componentOptions: {
         pdf: {
-          pdfjs: "/pdfjs",
+          pdfjs: "/pdfjs/web/viewer.html",
         },
       }
     },
